a2d8d6580bd869a55936b63ac88d12289b4ea2d3
Nathan Zimmerman <npzimmerman@gmail.com>
2026-04-20 17:20:16 -0500

    fish: auto-attach tmux on interactive SSH/mosh logins

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,12 @@
 export PATH="$HOME/.local/bin:$PATH"
 
 if status is-interactive
+    # Auto-attach tmux for interactive SSH/mosh logins.
+    # Skipped for local terminals ($SSH_CONNECTION unset) and non-interactive ssh cmd.
+    if test -z "$TMUX"; and test -n "$SSH_CONNECTION"; and type -q tmux
+        exec tmux new-session -A -s main
+    end
+
     export NVM_DIR="$HOME/.nvm"
     starship init fish | source
     atuin init fish | source
